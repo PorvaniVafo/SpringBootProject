10425d2cef84ed0d911820c18616522461d75dfe
PorvaniVafo <PorvaniVafo>
2024-03-23 23:16:11 +0600

    Brief description of changes

--- src/main/java/controller/StudentControler.java
+++ src/main/java/controller/StudentControler.java
@@ -3,6 +3,7 @@ package controller;
 import entity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import repository.StudentRepository;
 
@@ -11,11 +12,19 @@ import java.util.List;
 @RestController
 public class StudentControler {
 
+    //localhost:8080/students
     @Autowired
     StudentRepository repo;
     @GetMapping("/students")
     public List<Student> getAllStudents(){
         List<Student> students = repo.findAll();
-        return null;
+        return students;
     }
+    //localhost:8080/students/1
+    @GetMapping("/students/{id}")
+    public Student getStudent(@PathVariable int id){
+        Student student = repo.findById(id).get();
+        return student;
+    }
+
 }
